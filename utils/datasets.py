7451424278998a753de96ff0b298a66910fe1ff5
shariarriday <shariarriday@gmail.com>
2020-11-01 19:07:32 +0600

    Augmentations Disabled

--- utils/datasets.py
+++ utils/datasets.py
@@ -606,34 +606,34 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
             # if random.random() < 0.9:
             #     labels = cutout(img, labels)
 
-            transform = A.Compose([
-                A.OneOf([
-                    A.IAAAdditiveGaussianNoise(),
-                    A.GaussNoise(),
-                ], p=0.2),
-                A.OneOf([
-                    A.MotionBlur(p=.2),
-                    A.MedianBlur(blur_limit=3, p=0.1),
-                    A.Blur(blur_limit=3, p=0.1),
-                    A.MultiplicativeNoise(
-                        multiplier=[0.5, 1.5], per_channel=True, p=1)
-                ], p=0.2),
-                A.OneOf([
-                    A.CLAHE(clip_limit=2),
-                    A.IAASharpen(),
-                    A.IAAEmboss(),
-                    A.RandomBrightnessContrast(),
-                    A.RandomShadow(num_shadows_lower=1, num_shadows_upper=1,
-                                   shadow_dimension=5, shadow_roi=(0, 0.5, 1, 1), p=1),
-                    A.Cutout(num_holes=30, max_h_size=20,
-                             max_w_size=20, fill_value=127, p=1),
-                    A.RandomFog(fog_coef_lower=0.05, fog_coef_upper=.2),
-                    A.RandomRain(),
-                    A.ChannelShuffle(),
-                    A.ChannelDropout()
-                ], p=0.3),
-                A.HueSaturationValue(p=0.3),
-            ])
+            # transform = A.Compose([
+            #     A.OneOf([
+            #         A.IAAAdditiveGaussianNoise(),
+            #         A.GaussNoise(),
+            #     ], p=0.2),
+            #     A.OneOf([
+            #         A.MotionBlur(p=.2),
+            #         A.MedianBlur(blur_limit=3, p=0.1),
+            #         A.Blur(blur_limit=3, p=0.1),
+            #         A.MultiplicativeNoise(
+            #             multiplier=[0.5, 1.5], per_channel=True, p=1)
+            #     ], p=0.2),
+            #     A.OneOf([
+            #         A.CLAHE(clip_limit=2),
+            #         A.IAASharpen(),
+            #         A.IAAEmboss(),
+            #         A.RandomBrightnessContrast(),
+            #         A.RandomShadow(num_shadows_lower=1, num_shadows_upper=1,
+            #                        shadow_dimension=5, shadow_roi=(0, 0.5, 1, 1), p=1),
+            #         A.Cutout(num_holes=30, max_h_size=20,
+            #                  max_w_size=20, fill_value=127, p=1),
+            #         A.RandomFog(fog_coef_lower=0.05, fog_coef_upper=.2),
+            #         A.RandomRain(),
+            #         A.ChannelShuffle(),
+            #         A.ChannelDropout()
+            #     ], p=0.3),
+            #     A.HueSaturationValue(p=0.3),
+            # ])
 
             img = transform(image=img)['image']
 
